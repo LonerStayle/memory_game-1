1f4b285e273ba527793cdd74a9913d570e6e04d0
Loner123 <dlwlstjq410@gmail.com>
2020-07-24 16:41:08 +0900

    [추가] 프레그먼트, drwable, style, 액션바 제거등등

---
+++ app/src/main/java/com/example/memorygame/GameSettingFragment.kt
@@ -0,0 +1,25 @@
+package com.example.memorygame
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.memorygame.databinding.FragmentGameBinding
+import com.example.memorygame.databinding.FragmentGameSettingBinding
+
+class GameSettingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = DataBindingUtil.inflate<FragmentGameSettingBinding>(
+        inflater,
+        R.layout.fragment_game,
+        container,
+        false
+    ).run {
+        root
+    }
+}
